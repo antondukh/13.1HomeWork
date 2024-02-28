b406be8299a5e6918765f85907157fa5f6b8ae3e
antondukh <antondukh1993@gmail.com>
2024-02-28 20:17:59 +1000

    ///

---
+++ tests/test_code.py
@@ -0,0 +1,26 @@
+import pytest
+from class_category import Category
+from class_product import Product
+
+
+@pytest.fixture
+def class_category():
+    return Category('фрукты', 'фрукт', 'банан')
+
+
+def test_init_category(class_category):
+    assert class_category.name == 'фрукты'
+    assert class_category.description == 'фрукт'
+    assert class_category.goods == 'банан'
+
+
+@pytest.fixture
+def class_product():
+    return Product('Фуджи', 'яблоки', 105.56, 45)
+
+
+def test_init_product(class_product):
+    assert class_product.name == 'Фуджи'
+    assert class_product.description == 'яблоки'
+    assert class_product.price == 105.56
+    assert class_product.quantity == 45
