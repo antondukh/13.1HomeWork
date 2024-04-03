f39d73175586936cb2ad3dd5169bf87baaa6e02e
antondukh <antondukh1993@gmail.com>
2024-04-03 18:58:23 +1000

    16.1

--- class_category.py
+++ class_category.py
@@ -17,10 +17,30 @@ class Category:
         Category.total_categories += 1
         Category.total_products = len(self.__goods)
 
+    @property
+    def average_price(self):
+        """Gодсчитывает средний ценник всех товаров."""
+        quantity_all = 0
+        price_all = 0
+        for i in self.__goods:
+            quantity_all += i.quantity
+        for i in self.__goods:
+            price_all += i.price
+        try:
+            quantity_all = 0
+            result = price_all / quantity_all
+        except ZeroDivisionError:
+            return 0
+
+        return price_all / quantity_all
+
+
     def add_product(self, prod):
         """Добавление объекта товара в список."""
         if not isinstance(prod, Product):
             raise ValueError('Складывать можно только объекты одного класса')
+        elif prod.quantity == 0:
+            raise ValueError('товар с нулевым количеством не может быть добавлен')
         self.__goods.append(prod)
 
 
@@ -38,3 +58,13 @@ class Category:
 
     def __str__(self):
         return f"{self.name}, количество продуктов: {len(self)}"
+
+
+a = Category('фрукты', 'фрукт', [])
+b = Product('Фуджи', 'яблоки', 50, 1)
+f = Product('Фуджи', 'яблоки', 100, 1)
+a.add_product(b)
+a.add_product(f)
+
+print(a.average_price)
+
